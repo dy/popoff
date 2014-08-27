2e13d6db564c09fff6c274de68b7adf0e4d615c0
Dmitry <df.creative@gmail.com>
2014-08-27 16:30:52 +0400

    Build as a dependencies

--- dist/popup.js
+++ dist/popup.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var hasOwn = Object.prototype.hasOwnProperty;
 var toString = Object.prototype.toString;
 var undefined;
@@ -80,7 +80,7 @@ module.exports = function extend() {
 };
 
 
-},{}],2:[function(require,module,exports){
+},{}],"popup":[function(require,module,exports){
 /**
 * Extend poppy with popup behaviour
 */
@@ -250,4 +250,4 @@ proto.place = function(){
 
 //handle popup as a mod
 Mod(Popup);
-},{"extend":1,"mod-constructor":undefined,"placer":undefined,"poppy":undefined}]},{},[2]);
+},{"extend":1,"mod-constructor":undefined,"placer":undefined,"poppy":undefined}]},{},[]);
